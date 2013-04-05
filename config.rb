ae7edbb238063ee4230c8e5c5696cb20f1959a36
Rex McConnell <rex@rexmac.com>
2013-04-05 13:38:11 +0000

    Update Compass config to use new src and public directories

--- config.rb
+++ config.rb
@@ -1,8 +1,12 @@
 # Compass config
 
+# Publishing paths
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascript_dir = "js"
+css_dir = "public/css"
 
+# Development paths
+sass_dir = "src/sass"
+images_dir = "src/img"
+javascript_dir = "src/js"
+
+output_style = :compressed
