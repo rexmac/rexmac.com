facea04fa7f659843e6d307fe4a06a595e44d486
Rex McConnell <rex@rexmac.com>
2014-01-17 01:28:06 +0000

    Add new project page generator; add Pebble projects; re-organize projects

--- _plugins/projects.rb
+++ _plugins/projects.rb
@@ -2,27 +2,44 @@
 #
 # Original: https://github.com/BlackBulletIV/blackbulletiv.github.com/blob/master/_plugins/projects.rb
 #
+require 'json'
 require_relative 'custom_page'
+#require_relative 'slugize'
 
 module Jekyll
   class Project < CustomPage
-    def initialize(site, base, dir, name, info)
+    def initialize(site, base, dir, category, name)
       super site, base, dir, 'project'
-      puts "Building project: #{name}"
+      puts "Building project page: #{name}"
 
-      self.data['title'] = info['title'] || name
-      self.data['version'] = info['version_title'] || info['version']
-      self.data['repo'] = "https://github.com/#{site.config['github_user']}/#{name}"
-      self.data['download'] = "#{self.data['repo']}/zipball/#{info['version'] || 'master'}"
-      self.data['docs'] = info['docs'] == 'wiki' ? "#{self.data['repo']}/wiki" : info['docs'] if info['docs']
-      self.data['description'] = info['description']
+      github_name = (category == 'pebble' ? 'pebble-' : '') + name
+      releases = JSON.parse(`curl -sS https://api.github.com/repos/#{site.config['author']['github']}/#{github_name}/releases`)
+      latest_release = ""
+      latest_release_index = 0
+      releases.each_with_index do |obj, i|
+        unless obj.is_a?(Array) || latest_release > obj['tag_name']
+          latest_release = obj['tag_name']
+          latest_release_index = i
+        end
+      end
+      return if latest_release === ""
+      latest_release = releases[latest_release_index]
+
+      self.data['title'] = name
+      self.data['version'] = latest_release['name']
+      self.data['repo_url'] = "https://github.com/#{site.config['author']['github']}/#{github_name}"
+      self.data['tarball_url'] = "https://codeload.github.com/#{site.config['author']['github']}/#{github_name}/legacy.tar.gz/#{self.data['version']}"
+      self.data['zipball_url'] = "https://codeload.github.com/#{site.config['author']['github']}/#{github_name}/legacy.zip/#{self.data['version']}"
+      if category == 'pebble'
+        self.data['pbw_url'] = "#{self.data['repo_url']}/releases/download/#{self.data['version']}/#{latest_release['assets'][0]['name']}"
+      end
 
       # this stuff is bit hackish, but it works
-      readme = `curl https://raw.github.com/#{site.config['github_user']}/#{name}/master/README.md` # this will fail if README.md isn't present
+      readme = `curl -sS https://raw.github.com/#{site.config['author']['github']}/#{github_name}/#{latest_release['name']}/README.md`
       readme.gsub!(/\`{3} ?(\w+)\n(.+?)\n\`{3}/m, "{% highlight \\1 %}\n\\2\n{% endhighlight %}")
-      readme.gsub!(/Ö/, '&#214;') # so that I can use LÖVE in my READMEs
+      #readme.gsub!(/Ö/, '&#214;')
       readme = Liquid::Template.parse(readme).render({}, :filters => [Jekyll::Filters], :registers => { :site => site })
-      self.data['readme'] = RDiscount.new(readme).to_html
+      self.data['readme'] = Kramdown::Document.new(readme).to_html
     end
   end
 
@@ -40,17 +57,20 @@ module Jekyll
       return unless self.config.key? 'projects'
       throw "No 'project' layout found." unless self.layouts.key? 'project'
       dir = self.config['project_dir'] || 'projects'
-      projects = []
+      project_pages = []
 
-      self.config['projects'].each do |k, v|
-        slug = v['slug'] || k.slugize
-        p = Project.new(self, self.source, File.join(dir, slug), k, v)
-        p.data['url'] = "/#{dir}/#{slug}"
-        projects << p
-        write_page p
+      self.config['projects'].each do |category, projects|
+        projects.each do |name|
+          #slug = v['slug'] || k.slugize
+          slug = name.slugize
+          p = Project.new(self, self.source, File.join(dir, category, slug), category, name)
+          p.data['url'] = "/#{dir}/#{slug}"
+          project_pages << p
+          write_page p
+        end
       end
 
-      write_page Projects.new(self, self.source, dir, projects) if self.layouts.key? 'projects'
+      write_page Projects.new(self, self.source, dir, project_pages) if self.layouts.key? 'projects'
     end
   end
 end
--- src/projects/index.md
+++ src/projects/index.md
@@ -5,20 +5,42 @@ title: Projects
 
 ## <a id="digital"></a><a id="software"></a>Software
 
+#### <a id="node-js"></a>Node JS
+
 * [Chatbot](http://chatbot.rexmac.com/)
   * Demonstration of a very rudimentary chatbot implemented using [Node.JS](http://nodejs.org/) and [Socket.IO](http://socket.io/).
+  
+#### <a id="jquery"></a>jQuery
+
 * [jQuery Collapsible Fieldset plugin](http://github.com/rexmac/jquery-collapsibleFieldset)
   * Small jQuery plugin for collapsing fieldsets by clicking their legends
 * [jQuery Treemap plugin](http://rexmac.com/jquery/treemap)
   * A (simple) jQuery plugin for generating treemaps with (optional) heatmap-style coloring
-* [MTurk-Qualtrics Helper](/projects/mturk)
-  * Web app to assist with integration of [Amazon's Mechanical Turk](http://mturk.com/) and [Qualtrics](http://qualtrics.com/) surveys
-* [One-Click Class](http://oneclickclass.com/)
-  * Just what the name says
+  
+#### <a id="pebble"></a>Pebble Smartwatch
+
+* [ChronoCode](/chronocode)
+  * A watchface for the Pebble Smartwatch featuring mutli-lingual blocks of words with "hidden" phrases for displaying the time.
+
+* [PingChrong](/pingchrong)
+  * A ping-pong inspired watchface for the Pebble Smartwatch. Based on the [MONOCHRON clock](http://www.adafruit.com/products/204) by [Adafruit](http://www.adafruit.com/).
+
+#### <a id="php"></a>PHP
+
 * [Zyndax](http://github.com/rexmac/zyndax)
   * An outdated (and obsolete?) library extending [ZendFramework 1](http://framework.zend.com/) to ease development of ZF-based web projects.
+  
+#### <a id="just-for-fun"></a>Just for Fun
 
-## <a id="analog"></a><a id="hardware"></a>Hardware
+* [One-Click Class](http://oneclickclass.com/)
+  * Just what the name says
+  
+#### <a id="misc"></a>Misc
+
+* [MTurk-Qualtrics Helper](/projects/mturk)
+  * Web app to assist with integration of [Amazon's Mechanical Turk](http://mturk.com/) and [Qualtrics](http://qualtrics.com/) surveys
+
+### <a id="analog"></a><a id="hardware"></a>Hardware
 
 In the future, I hope to have dedicated pages for each of the projects below with notes and photos of my personal experiences. For now, I am linking to the pages where I purchased the kits or first heard of the projects.
 
