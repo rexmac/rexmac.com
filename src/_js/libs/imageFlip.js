57c44bb17c1527a46997bc1d85a1812652a5b708
Rex McConnell <rex@rexmac.com>
2014-01-18 15:38:56 +0000

    Swap position of profile photos

--- src/_js/libs/imageFlip.js
+++ src/_js/libs/imageFlip.js
@@ -7,12 +7,12 @@ if(!Modernizr.csstransforms3d) {
       el.flip({
         direction: 'lr',
         //content: el.find('.back.face').html()
-        content: '<img src="/img/profile.photo.jpg" />'
+        content: '<img src="/img/profile.pixel.png" />'
       });
     } else {
       el.flip({
         direction: 'rl',
-        content: '<img src="/img/profile.pixel.png" />'
+        content: '<img src="/img/profile.photo.jpg" />'
       });
     }
   });
